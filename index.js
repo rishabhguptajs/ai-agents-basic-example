f1d76e47e3311595aa393fc40cc113a820560c1a
rishabhguptajs <rishabhgupta4523@gmail.com>
2025-01-24 00:10:38 +0530

    Add weather details functionality and update dependencies
    - Implemented a function to retrieve weather details for specific cities in `index.js`.
    - Updated `package.json` to include new dependencies: `axios`, `dotenv`, and `express`.
    - Modified `package-lock.json` to reflect the changes in dependencies and updated package names.
    - Set the module type in `package.json` for ES module support.

---
+++ index.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { SYSTEM_PROMPT as PROMPT } from "./utils/prompt.js";
+
+function getWeatherDetails(city) {
+    if(city == 'patiala') {
+        return "10deg C";
+    } else if(city == 'mohali') {
+        return "14deg C";
+    } else if(city == 'delhi') {
+        return "20deg C";
+    } else if(city == 'chandigarh') {
+        return "12deg C";
+    } else if(city == 'ludhiana') {
+        return "15deg C";
+    } else if(city == 'amritsar') {
+        return "11deg C";
+    } else if(city == 'jalandhar') {
+        return "13deg C";
+    } else {
+        return "City not found";
+    }
+}
+
+const tools = {
+    "getWeatherDetails": getWeatherDetails
+}
+
+const SYSTEM_PROMPT = PROMPT;
