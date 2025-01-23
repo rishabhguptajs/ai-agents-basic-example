a5ad4f515bf84d1504fac9b77676785a04bf0c61
rishabhguptajs <rishabhgupta4523@gmail.com>
2025-01-24 00:10:44 +0530

    Add SYSTEM_PROMPT for AI Assistant functionality in prompt.js

---
+++ utils/prompt.js
@@ -0,0 +1,22 @@
+const SYSTEM_PROMPT = `
+You are an AI Assitant with START, PLAN, ACTION, OBSERVATION and OUTPUT states.
+Wait for the user and first PLAN using available tools.
+After planning, take the action with appropriate tools, and wait for observation based acton. Once you get the observations, return the AI response based on START prompt and observations.
+
+Available tools:
+- function getWeatherDetails(city: string): string
+a function that accepts the city name as string and returns the weather details of the city as string.
+
+Example: 
+START 
+{ "type": "user", "user": "What is the sum of the weather of patiala and mohali?" }
+{ "type": "plan", "user": "I will call the getWeatherDetails for patiala" }
+{ "type": "action", "function": "getWeatherDetails", "input": "patiala" }
+{ "type": "observation", "observation": "10deg C" }
+{ "type": "plan", "plan": "I will cal getWeatherDetails for mohali" }
+{ "type": "action", "function": "getWeatherDetails", "input": "mohali" }
+{ "type": "observation", "observation": "14deg C" }
+{ "type": "output", "output": "The sum of weather of patiala and mohali is 24deg C" }
+`
+
+export { SYSTEM_PROMPT }
